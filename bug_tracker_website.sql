ae965e2add6f78a3e941ba34305f31e042d5fd70
JanLancelot <132678636+JanLancelot@users.noreply.github.com>
2023-05-26 23:26:11 +0800

    Update bug_tracker_website.sql

--- bug_tracker_website.sql
+++ bug_tracker_website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2023 at 06:14 AM
+-- Generation Time: May 26, 2023 at 05:23 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `bug_tracker_website`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attachments`
+--
+
+CREATE TABLE `attachments` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `bug_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -46,11 +59,130 @@ CREATE TABLE `bugs` (
 
 INSERT INTO `bugs` (`id`, `title`, `description`, `status`, `priority`, `created_at`, `updated_at`, `creator_id`, `assignee_id`, `project_id`) VALUES
 (5, 'Wrong redirection upon bug deletion', 'def delete_bug() redirects the user to the dashboard, preferably, it should be back into the current selected project.', 'in_progress', 'medium', '2023-05-13 05:24:38', '2023-05-13 19:58:54', 5, NULL, 3),
-(14, 'Test', 'Test', 'open', 'low', '2023-05-13 17:36:42', '2023-05-14 01:36:42', 5, 1, 3);
+(14, 'Test', 'Test', 'open', 'low', '2023-05-13 17:36:42', '2023-05-14 01:36:42', 5, 1, 3),
+(25, 'Test High Bug', 'Test', 'in_progress', 'high', '2023-05-26 04:56:04', '2023-05-26 12:56:04', 1, 5, 3),
+(26, 'Test Critical Bug', 'Critical', 'in_progress', 'critical', '2023-05-26 04:56:26', '2023-05-26 12:56:26', 1, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) DEFAULT NULL,
+  `bug_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `content`, `created_at`, `user_id`, `bug_id`) VALUES
+(1, 'Test Comment', '2023-05-13 05:55:29', 5, 5),
+(2, 'Test 2', '2023-05-13 05:58:56', 5, 5),
+(3, 'Test Comment', '2023-05-13 17:38:16', 1, 14),
+(4, 'Test Comment 3', '2023-05-13 19:59:05', 5, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT current_timestamp(),
+  `read` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `content`, `user_id`, `created_at`, `read`) VALUES
+(1, 'New bug assigned to you: sss', 5, '2023-05-14 02:16:57', 0),
+(2, 'You have been unassigned from bug: Wrong redirection upon bug deletion', 5, '2023-05-14 03:20:40', 0),
+(3, 'New bug assigned to you: Wrong redirection upon bug deletion', NULL, '2023-05-14 03:20:40', 0),
+(4, 'New bug assigned to you: Test Graph', 1, '2023-05-19 13:48:33', 0),
+(5, 'New bug assigned to you: Test Profile', 5, '2023-05-20 08:41:01', 0),
+(6, 'New bug assigned to you: Test 2', 1, '2023-05-22 08:00:41', 0),
+(7, 'New bug assigned to you: Test High Bug', 5, '2023-05-26 12:56:04', 0),
+(8, 'New bug assigned to you: Test Critical Bug', 1, '2023-05-26 12:56:26', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_by_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `name`, `description`, `created_at`, `updated_at`, `created_by_id`) VALUES
+(3, 'Bug Tracker Website', 'Project in DAS', '2023-05-12 18:33:14', '2023-05-26 04:58:54', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_members`
+--
+
+CREATE TABLE `project_members` (
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` enum('admin','project_manager','developer','tester') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password_hash`, `role`) VALUES
+(1, 'Goyong', 'janlancelotm@gmail.com', '$2b$12$UI063/TfDyoKQT/cH7KenuKlyktqj4cgaU2ylM/vRGJPHuH8vstFe', 'admin'),
+(5, 'Lancelot', 'maypagasaml@gmail.com', '$2b$12$MtjdUsKxtOfoQlFGDZ5CfOO8cLaBMpsOUKbfgxxztSZkzwiRIhdx6', 'project_manager'),
+(6, 'lialialia', 'macecilia.santiagooo@gmail.com', '$2b$12$gWSGUPaVLGrRpbV.hQOO5eItRQwcMsLOpUin4mbEE1/ILcfZ/FR62', 'admin'),
+(7, 'sorianoleah', 'leahsoriano405@gmail.com', '$2b$12$KHuRClbP7qkWxujbxa23TejFW.LYVA42zbND7UttbkqLgiR5F0jmS', 'admin'),
+(8, 'Mathew', 'castillo.jmrafael.01045@dyci.edu.ph', '$2b$12$jvaefamjV/VARsVRiRJb1eibQsLSLtWGuddKzMV12Ry1/6TP24jKG', 'admin');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attachments`
+--
+ALTER TABLE `attachments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bug_id` (`bug_id`);
 
 --
 -- Indexes for table `bugs`
@@ -62,18 +194,91 @@ ALTER TABLE `bugs`
   ADD KEY `project_id` (`project_id`);
 
 --
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `bug_id` (`bug_id`);
+
+--
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `created_by_id` (`created_by_id`);
+
+--
+-- Indexes for table `project_members`
+--
+ALTER TABLE `project_members`
+  ADD PRIMARY KEY (`user_id`,`project_id`),
+  ADD KEY `project_id` (`project_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `attachments`
+--
+ALTER TABLE `attachments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `bugs`
 --
 ALTER TABLE `bugs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `attachments`
+--
+ALTER TABLE `attachments`
+  ADD CONSTRAINT `attachments_ibfk_1` FOREIGN KEY (`bug_id`) REFERENCES `bugs` (`id`);
 
 --
 -- Constraints for table `bugs`
@@ -82,6 +287,32 @@ ALTER TABLE `bugs`
   ADD CONSTRAINT `bugs_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `bugs_ibfk_2` FOREIGN KEY (`assignee_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `bugs_ibfk_3` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
+
+--
+-- Constraints for table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`bug_id`) REFERENCES `bugs` (`id`);
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `projects`
+--
+ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`created_by_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `project_members`
+--
+ALTER TABLE `project_members`
+  ADD CONSTRAINT `project_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `project_members_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
